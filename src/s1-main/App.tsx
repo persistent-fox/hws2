03ad598154c6f700bb4df7b5fa595c9e4f3831f8
Margaret <rixten.6@gmail.com>
2024-07-11 20:51:22 +0300

    add hw 7

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './App.module.css';
 import HW5 from '../s2/hw05/HW5';
 import HW6 from '../s2/hw06/HW6';
+import HW7 from '../s2/hw07/HW7';
 
 function App() {
 	return (
@@ -13,8 +14,9 @@ function App() {
 			<HW4 /> */}
 
 			{/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
-			<HW5 />
-			<HW6 />
+			{/* <HW5 />
+			<HW6 /> */}
+			<HW7 />
 		</div>
 	);
 }
